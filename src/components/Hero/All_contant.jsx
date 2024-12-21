c2cbd1754480c744104476dab71e7b5b01f423ec
iamramanverma <iamramanvermaa@gmail.com>
2024-12-21 16:37:00 +0530

    Merged changes from <hero>

--- src/components/Hero/All_contant.jsx
+++ src/components/Hero/All_contant.jsx
@@ -1,7 +1,63 @@
 import "../../index.css";
 
-import { useState } from "react";
+import {useState } from "react";
 const All_contant = () => {
+
+
+  const [cards, setCards] = useState([
+    { img: "./images/It_services/it_services.jpg",
+      cname: "IT SERVICE",
+      summary:"IT services encompass a wide range of technological solutions designed to support businesses and individuals in managing, optimizing, and securing their digital operations. These services include consulting, infrastructure management ....",
+      id: 1,
+      flipped: false 
+    },
+
+    { img: "./images/It_services/commmm.jpg",
+      cname: "Comminucation",
+      summary:"Communication services enable the seamless, efficient, and fast exchange of information between individuals and businesses. These services include email, messaging, voice and video calling, data sharing, and team collaboration tools. Modern communication leverages cloud platforms...",
+      id: 2,
+      flipped: false
+    },
+
+    { img: "./images/It_services/security.jpg",
+      cname: "Security System",
+      summary:" security system is designed to protect individuals, properties, and information from potential threats such as theft, unauthorized access, or cyberattacks. It encompasses a range of solutions, including surveillance cameras, alarm systems, access control devices....",
+      id: 3,
+      flipped: false 
+    },
+
+    { img: "./images/It_services/networking.jpg",
+      cname: "Networking",
+      summary:" Networking refers to the process of connecting computers, devices, and systems to facilitate communication and the exchange of data. It forms the backbone of modern technology, enabling seamless interaction between users, applications, and services....",
+      id: 4, 
+      flipped: false 
+    },
+
+    { img: "./images/It_services/it_connn.jpg",
+      cname: "IT Consulting",
+      summary:" IT consulting involves providing expert advice and guidance to organizations on how to leverage technology to achieve their business goals. IT consultants assess an organization’s current technological infrastructure, identify areas for improvement....",
+      id: 5, 
+      flipped: false 
+    },
+
+    { img: "./images/It_services/it_support.jpg",
+      cname: "IT Support",
+      summary:" IT support refers to the services provided to assist individuals and organizations in maintaining and troubleshooting their technology systems. It encompasses a wide range of activities, including helping users resolve hardware or software issues, managing system....",
+      id: 6, 
+      flipped: false 
+    },
+  ]);
+
+  const [flippedCard, setFlippedCard] = useState(null)
+  const handleFlip = (id) => {
+    if(flippedCard === id){
+      setFlippedCard(null)
+    }else{
+      setFlippedCard(id);
+    }
+    
+  }
+
   
   const content = [
     {
@@ -50,34 +106,40 @@ const All_contant = () => {
 
   return (
     <>
-      <div id="cursor_none" className=" flex flex-wrap  justify-evenly ">
-        {content.map((item, index) => {
-          return (
-            <>
-              <div
-                id="box_shadow"
-                className=" rounded-3xl w-[26%]  overflow-hidden my-20 bg-[#e8e8e8] "
-              >
-                <img src={item.img} alt="" className="h-96 w-full " />
-                <h1 className=" ml-8 text-3xl font-semibold my-4 underline underline-offset-4">
-                  {item.cname}
-                </h1>
-                <p
-                  className="px-4  text-2xl text-[#444343] w-[98%] text-justify leading-8
-                 tracking-tight hover:text-black ease-in duration-1000 overflow-hidden  h-[144px]"
-                >
-                  {item.summary}
-                </p>
-                <div className="bg-black h-20 text-center pt-3">
-                  <button className=" text-black px-4  py-2 rounded-3xl  bg-[#b7b7b7] shadow-lg shadow-blue-500/50  hover:bg-white hover:text-[#068FFF] ease-linear duration-500">
-                    {item.btn}
-                  </button>
-                </div>
+    <div className="cards-container">
+      {cards.map((card) => (
+        <div
+          key={card.id}
+          className={`card-container ${flippedCard === card.id ? 'flipped' : ''}`}
+          
+        >
+          <div className="card">
+            <div className="card-front">
+              <img className="h-96 w-full " src={card.img} alt="img" />
+              <h1 className=" ml-8 text-3xl font-semibold my-4 underline underline-offset-4">{card.cname}</h1>
+              <p className="px-4  text-2xl text-[#444343] w-[98%] text-justify leading-8
+                 tracking-tight hover:text-black ease-in duration-1000 overflow-hidden h-[145px]">
+                  {card.summary}
+                  </p>              
+              <div>
+              <button onClick={() => handleFlip(card.id)} className=" text-white mx-4 px-4  py-2 rounded-3xl  bg-[#475764] shadow-lg shadow-cyan-500/50  hover:bg-white hover:text-[#068FFF] ease-linear duration-1000">see more</button>
               </div>
-            </>
-          );
-        })}
+            </div>
+            <div className="card-back" onClick={() => handleFlip(card.id)}>
+            <p className="px-4  text-2xl text-black w-[98%] text-justify leading-8
+                 tracking-tight  ease-in duration-1000 overflow-hidden h-[145px]">
+                  {card.summary}
+                  </p> 
+            </div>
+          </div>
+        </div>
+      ))}
       </div>
+  
+
+
+
+
 
       <div className="  bg-[#edeeee]">
         <marquee behavior="" direction="">
